f3e7b792eeabd5576dd27b36a24e0b15c4fda9af
sorayanfurter <sorinfurter@gmail.com>
2023-10-18 00:17:32 -0300

    User, Address, WebOrder, Quantities and Inventory Entities

---
+++ src/main/java/com/project/ecommv2backend/Ecommv2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommv2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommv2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommv2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecommv2backend/Ecommv2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecommv2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommv2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
